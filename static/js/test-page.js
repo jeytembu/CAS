f40dc37a12c535020e5ba89189ddb7a85048f023
jeytembu <jeytembu@gmail.com>
2016-11-19 09:13:46 +0300

    modified the script on the progress bar in the test page

---
+++ static/js/test-page.js
@@ -0,0 +1,6 @@
+function progress(){
+    var elem = document.getElementById('myBar');
+    var wid = window.getComputedStyle(elem, null).getPropertyValue("width");
+    width = 100%
+    elem.style.width = width + '%'; 
+}
